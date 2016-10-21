1c8a99d352b5067fb1748b04184c4249d54343cf
YongaJ <yongajacobs@gmail.com>
2016-10-21 16:01:44 +0200

    Add files via upload

---
+++ Services/OAuthMessageHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Twitter24.Models;
+using System.Configuration;
+
+namespace Twitter24.Services
+{
+    /* Basic DelegatingHandler that creates an OAuth authorization header based on the OAuthBase 
+     * Class downloaded from http://oauth.net */
+    public class OAuthMessageHandler : DelegatingHandler
+    {
+
+        /* Twitter OAuth values are stored in TwitterSettings.config. */
+
+        /* Retrieve consumer key */
+        private static string _consumerKey = ConfigurationManager.AppSettings["consumerkey"].ToString();
+
+        /* Retrieve consumer secret */
+        private static string _consumerSecret = ConfigurationManager.AppSettings["consumersecret"].ToString();
+
+        /* Retrieve access token */
+        private static string _token = ConfigurationManager.AppSettings["accesstoken"].ToString();
+
+        /* Retrieve access secret */
+        private static string _tokenSecret = ConfigurationManager.AppSettings["accesssecret"].ToString();
+
+        private OAuthBase _oAuthBase = new OAuthBase();
+
+        public OAuthMessageHandler(HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            /* Compute OAuth header */
+            string normalizedUri;
+            string normalizedParameters;
+            string authHeader;
+
+            string signature = _oAuthBase.GenerateSignature(request.RequestUri, _consumerKey, _consumerSecret, _token, _tokenSecret, request.Method.Method, _oAuthBase.GenerateTimeStamp(), _oAuthBase.GenerateNonce(), out normalizedUri, out normalizedParameters, out authHeader);
+
+            request.Headers.Authorization = new AuthenticationHeaderValue("OAuth", authHeader);
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ Services/TwitterRestService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Twitter24.Models;
+
+namespace Twitter24.Services
+{
+    public class TwitterRestService
+    {
+        private static string _address = "https://api.twitter.com/1.1/statuses/user_timeline.json?include_rts=true&screen_name=News24&count=20";
+
+        public Tweets _SingleTweet { get; set; }
+        public List<Tweets> _Tweets { get; set; }
+
+        public async Task<List<Tweets>> GetUserTimeline()
+        {
+            /* initialize with 0 item */
+            _Tweets = new List<Tweets>();
+
+            /* Create client and insert an OAuth message handler in the message path that inserts an OAuth authentication header in the request. */
+            HttpClient client = new HttpClient(new OAuthMessageHandler(new HttpClientHandler()));
+
+            /* Send asynchronous request to twitter and read the response as JToken */
+            HttpResponseMessage response = await client.GetAsync(_address);
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                JToken _TwitterFeed = await response.Content.ReadAsAsync<JToken>();
+                foreach (var tweet in _TwitterFeed)
+                {
+                    /* Deserialization token */
+                    _SingleTweet = tweet.ToObject<Tweets>();
+
+                    /* Add a new item at the end of the list */
+                    _Tweets.Add(_SingleTweet);
+
+                }
+            }
+            return _Tweets;
+        }
+
+    }
+
+    
+}
