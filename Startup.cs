83cffd4a0d94db09657d7181b0e397101b502f7f
YongaJ <yongajacobs@gmail.com>
2016-10-21 15:27:55 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Twitter24.Startup))]
+
+namespace Twitter24
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
