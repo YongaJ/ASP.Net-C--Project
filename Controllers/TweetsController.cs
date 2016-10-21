cf596e8529ca3b558ccac8225336a6d975b401f2
YongaJ <yongajacobs@gmail.com>
2016-10-21 15:56:55 +0200

    Add files via upload

---
+++ Controllers/TweetsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Twitter24.Models;
+using Twitter24.Services;
+
+namespace Twitter24.Controllers
+{
+    public class TweetsController : ApiController
+    {
+        private TwitterRestService _TwitterRestService;
+
+        public TweetsController()
+        {
+            this._TwitterRestService = new TwitterRestService();
+        }
+        public async Task<List<Tweets>> GetTweets()
+        {
+            return await this._TwitterRestService.GetUserTimeline();
+        }
+        
+    }
+}
